d6584d02325184cbda391db29c96ee4e316b9851
Daniel Richard <daniel.richard@starcorp.com.br>
2020-08-30 02:15:10 -0300

    Montagem de arquitetura do projeto + swagger

---
+++ contrato-prestacao-api/Controllers/ContratoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace contrato_prestacao_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContratoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Teste()
+        {
+            return Ok(new { Sucess = "true" });
+        }
+    }
+}
---
+++ contrato-prestacao-api/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace contrato_prestacao_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Contrato Prestação API",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Daniel Richard",
+                            Url = new Uri("https://github.com/DanielRichard-Dev")
+                        }
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                    "Contrato Prestação API");
+            });
+        }
+    }
+}
